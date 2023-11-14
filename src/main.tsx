9bb815e959b23caf378f4df1d268e8ad8a6e6da8
Dmitry Bolotin <DBolotin@croc.ru>
2023-11-14 20:33:03 +1000

    reset project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as Element).render(
   <React.StrictMode>
-    <App />
+    <div>RESET PROJECT</div>
   </React.StrictMode>,
 )
